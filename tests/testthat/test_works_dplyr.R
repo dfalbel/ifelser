6061f7e30c11cca3de62bd4393bf0404bc0f56a9
Daniel Falbel <dfalbel@gmail.com>
2015-05-11 10:28:42 -0400

    corrigindo testes

--- tests/testthat/test_works_dplyr.R
+++ tests/testthat/test_works_dplyr.R
@@ -1,21 +1,31 @@
 context("Testing if ifelser works with dplyr")
 
 test_that("test_if , if_yes, if_no works inside dplyr", {
-  library(dplyr)
+  suppressPackageStartupMessages(library(dplyr))
 
   x <- mtcars %>%
     mutate(cyl_txt = test_if(cyl == 4) %>%
-                    if_true('Four') %>%
-                    if_false('Not Four'))
+             if_true('Four') %>%
+             if_false('Not Four'))
 
   y <- mtcars
   y$cyl_txt <- test_if(y$cyl == 4) %>%
     if_true('Four') %>%
     if_false('Not Four')
 
+  z <- mtcars
+  z$cyl_txt <- ifelse(z$cyl == 4, "Four", "Not Four")
+
+
   expect_identical(
-    x$cyl_text,
-    y$cyl_text
+    x$cyl_txt,
+    y$cyl_txt
   )
+
+  expect_identical(
+    x$cyl_txt,
+    z$cyl_txt
+  )
+
 
 })
